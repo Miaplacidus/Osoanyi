2a742f3be509f20080d94253610928b21616f5c0
Ifu Aniemeka <ianiemeka@gmail.com>
2017-09-25 20:51:23 -0500

    Send run data from server to client

--- client/components/run/index.jsx
+++ client/components/run/index.jsx
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Form from './form.jsx';
 import Runs from './runs.jsx';
+import axios from 'axios';
 
 export default class Osoanyi extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       runs: [],
     };
+
+  axios({
+    method:'get',
+    url:'http://localhost:3000/api'
+  })
+  .then(function(response) {
+    // this.state.runs.push(response);
+    // this.setState({ runs: this.state.runs });
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
     this.handleRunAdd = this.handleRunAdd.bind(this);
   }
@@ -17,7 +32,7 @@ export default class Osoanyi extends Component {
     this.state.runs.push(run);
     this.setState({ runs: this.state.runs });
   }
-  
+
   render(state) {
     return (
       <div>
--- server/src/main.go
+++ server/src/main.go
@@ -10,18 +10,22 @@ import (
 type Run struct {
     Level      string
     Duration   string
-    Time       string
+    Time       time.Time
 }
 
 type Runs []Run
 
 func main(){
-  runs := Runs{
-          Run{Level: "6 min mile", Duration: "1.5 hours", Time: "2:00 pm"},
-          Run{Level: "15 min mile", Duration: "Forever", Time: "6:30 pm"}
-      }
+  runs := Runs {
+            Run {Level: "6 min mile", Duration: "1.5 hours", Time: time.Now()},
+            Run {Level: "15 min mile", Duration: "Forever", Time: time.Now()},
+          }
 
-  http.HandleFunc("/", func (writer http.ResponseWriter, reader *http.Request)  {
+  http.HandleFunc("/api", func (writer http.ResponseWriter, reader *http.Request)  {
+    writer.Header().Set("Access-Control-Allow-Origin", "*")
+    writer.Header().Set("Content-Type", "application/json, text/plain")
+    writer.Header().Set("Access-Control-Allow-Methods", "GET")
+
     json.NewEncoder(writer).Encode(runs)
   })
 
